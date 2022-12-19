0329f0316e846d447f15e56971661682a98cfe11
parteekdcrust <pgarg56023@gmail.com>
2022-12-19 20:07:09 +0530

    initial update

---
+++ build/precache-manifest.334b5e4fe06a27586a8ff95dd5aa6337.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43847105c853bf5e21998f7909ea99ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "49f5ce94f497c1f54fc3",
+    "url": "/static/js/main.48eac93c.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
